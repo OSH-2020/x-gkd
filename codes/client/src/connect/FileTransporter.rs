27e36621893289f678658287c165884b90187bc6
QuYang020 <1602040838@qq.com>
2020-05-17 09:48:13 +0800

    update FileTransporter.rs

---
+++ codes/client/src/connect/FileTransporter.rs
@@ -0,0 +1,54 @@
+use std::io::{Stdin, Stdout};
+use std::io::prelude::*;
+use std::fs::File;
+
+pub fn send_file(mut f:File)->bool{
+    //将文件全部发送到stdout
+    std::io::stdout().flush();
+
+    let mut send_bytes = [0; 1024];
+    let mut buffer = Vec::new();
+    //let mut length = f.read_to_end(&mut buffer);
+
+    let mut length = f.read(&mut send_bytes[..]);
+    let length = match length{
+        Ok(len) => len as i64,
+        Err(error) => return false,
+    };
+    std::io::stdout().write_fmt(format_args!("{}\n", length));
+    std::io::stdout().flush();
+    std::io::stdout().write(&mut buffer);
+    std::io::stdout().flush();
+    
+    return true
+}
+
+pub fn recv_file(mut f:File, mut soc_in:Stdin)->bool{
+    //从stdin中读取并存储到文件中。
+    //原java文件中socout这个参数并没有用到，此处删去
+    //手动实现读取一个long类型的数据
+    let mut buffer = [0; 8];
+    soc_in.read_exact(&mut buffer).unwrap();
+    //Java 数据传输都是big endian，此处也默认读到数据是big endian
+    //from_bytes is a nightly-only experimental API.
+    //let file_length = i64::from_bytes(buffer);
+    let file_length:i64 = ((buffer[0] as i64) << 56) + (((buffer[1] as i64) & 255) << 48) + (((buffer[2] as i64) & 255) << 40)       
+     + (((buffer[3] as i64) & 255) << 32) + (((buffer[4] as i64) & 255) << 24) + (((buffer[5] as i64) & 255) << 16)        
+     + (((buffer[6] as i64) & 255) << 8) + (((buffer[7] as i64) & 255) << 0);
+    
+    let mut read:i64 = 0;
+    let mut r:i64 = 0;
+    let mut send_bytes = [0; 1024];
+    while read < file_length{
+        let read_res = soc_in.read(&mut send_bytes[..]);
+        let r = match read_res{
+            Ok(read_once) => read_once as i64,
+            Err(error) => panic!("Problem receieve file: {:?}", error),
+        };
+        read = read + r;
+        f.write(&send_bytes);//写入的数据量不能控制
+        f.flush();
+    }
+    //没有再创建FileOutputStream对象，这里不需要关闭什么
+    return true
+}//TODO:err handle
---
+++ temp/records/qy.md
@@ -0,0 +1,37 @@
+### ServerConnecter.rs
+
+目前为1.0版。这个文件定义了 ServerConnect 结构体，以及 new, init, run, stopConnect 四个方法。这个文件可以通过编译，但还有一些 warning 没有解决。
+
+* 关于 crate 结构如何组织的问题还没有解决。目前先按照原来项目的目录存放文件。有新的方案之后可能会需要在代码中增加几行。
+
+* 对应原文件的类 syn 字段在这里没有实现。目前我不了解 syn 在这里的作用和必要性。查找到 Rust 有 `Trait std::marker::Sync` 。
+
+  https://moshg.github.io/rust-std-ja/std/marker/trait.Sync.html
+
+* 还没有写部分函数的错误处理。目前多用 unwrap 或 expect 进行简单的错误处理，一出错就会调用 panic 关闭程序，还需要进一步修改区分一些可恢复法错误。
+
+* 使用 Bufreader 对 TcpStream 进行按行读的操作。为了同时使用 BufReader 读和使用原有的 TcpStream 写，在创建 BufReader 对象前调用了 socket.try_clone() ，对于这个函数的一些细节还不是很清楚。 
+
+* 没有查到原 ServerConnecter.java 中的 client.Client.getRS() 的作用及其对应方法，原文件第 68 行的调用没有对应实现。
+
+* 有涉及其他文件中定义的结构体的情况，方法接口可能不一致。目前写为：
+
+  ~~~rust
+  let f_manager = FragmentManager::new(request_id, fragment_id, ftype);
+  f_manager.submit();
+  ~~~
+
+  注：type 是 Rust 关键字，因此此处变量命名为 ftype 。
+
+
+
+### FileTransporter.rs
+
+这个文件定义了 recv_file 和 send_file 两个函数。目前可以通过编译，还没有通过测试。
+
+* 这两个收发文件的函数是可以测试的，但我总是不能成功运行 read 函数，报错`thread 'main' panicked at 'Problem read file: Os { code: 5, kind: PermissionDenied, message: "拒绝访问。" }', src\main.rs:19:23`
+
+* 错误处理待完善
+* 我没有找到和 DataInputStream DataOutputStream 对应的处理方式，目前使用输出到 stdout ，输入到 stdin 的方式实现。
+* 目前接收文件是1024字节一次，分多次接收。发送文件是一次性发送。
+* 还没有找到发送文件时指定写入字节数的方法，有待更改。
