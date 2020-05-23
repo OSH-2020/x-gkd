c5a4ee8146e2bc8d3b4be28c7b0f3b093164f315
Elaine919 <58732052+Elaine919@users.noreply.github.com>
2020-05-23 16:39:29 +0800

    Update FileAttrs.rs

--- codes/client/src/fileDetector/FileAttrs.rs
+++ codes/client/src/fileDetector/FileAttrs.rs
@@ -1,20 +1,16 @@
-crate fileDetector
-
-/**
- * 文件属性
- */
 struct FileAttrs {
     name: String,
     path: String,
     attr: String,
     noa: i32,
 }
-fn FileAttrs(name: String, path: String,
-    attr: String, noa: i32) -> Self {
-    FileAttrs {
-        name: name,
-        path: path,
-        attr: attr,
-        noa: noa,
+impl FileAttrs {
+    fn init(name: String, path: String, attr: String, noa: i32) -> Self {
+        FileAttrs {
+            name: name,
+            path: path,
+            attr: attr,
+            noa: noa,
+        }
     }
 }
