176d839f24db0e4ff2d7ff51cc65c97d7c2d57b7
QuYang020 <1602040838@qq.com>
2020-06-03 10:07:02 +0800

    update server/controlConnect

---
+++ codes/server/src/controlConnect/ServerThread.rs
@@ -0,0 +1,27 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+struct ServerThread {
+    server:TcpListener,
+}
+
+impl ServerThread{
+    pub fn new(addr: String)->ServerThread{
+        let listener = TcpListener::bind(addr).unwrap();
+        println!("control socket setup!");
+        ServerThread{
+            server:listener,
+        }
+    }
+
+    pub fn run(&self){
+        loop{
+            let (socket, addr) = self.server.accept().unwrap();
+            println!("accepted a control link!");
+            thread::spawn(||{
+                let mut client = ClientThread::new(socket);
+                client.run();
+            });
+        }
+    }
+}
