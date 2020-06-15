29dc40ddea01f414a6996cebc23e6e3e94ecad92
pqz <peiqz@mail.ustc.edu.cn>
2020-06-15 16:53:22 +0800

    update FragmentManager.rs

--- codes/client/src/connect/FragmentManager.rs
+++ codes/client/src/connect/FragmentManager.rs
@@ -1,18 +1,22 @@
-use std::net::{TcpStream, Ipv4Addr, Shutdown};
+//use std::net::{TcpStream, Ipv4Addr, Shutdown};
+use std::net::TcpStream;
 use std::string::String;
 use std::io::BufReader;
 use std::io::Write;
 use std::io::prelude::*;
-use std::{thread, time};
+//use std::{thread, time};
 use std::fs::{File, remove_file};
-use std::ptr::null;
+//use std::ptr::null;
 use std::path::Path;
+//use std::option::NoneError;
+use std::option::Option;
 
 
 struct FragmentManager{
-    fragmentFolder : Path,
+    fragmentFolder : File,
     serverIP : String,
     serverPort : i32,
+    controlPort : u16,
     toServer : Option<TcpStream>,
     inFromServer : BufReader<TcpStream>,
     //DateOut/inputStream ??
@@ -22,22 +26,23 @@ struct FragmentManager{
 }
 
 
-impl FragmentManager(){
-
-
-    pub fn new(rId : i32, fId : i32, t : i32, tmp : Path){
+impl FragmentManager {
+    pub fn new(rId : i32, fId : i32, t : i32, f : File, l : TcpStream)->FragmentManager{
         FragmentManager{
+            fragmentFolder : f,
             serverIP : String :: new(),
             serverPort : -1,
+            controlPort : 0,
             toServer : None,
+            inFromServer: BufReader :: new(l),
             requestID : rId,
             fragmentID : fId,
             Type : t//type为Rust关键字，改为大写开头
         }
     }
 
-    pub fn init0(&self,tmp : Path){
-        self.FragmentFolder = tmp;
+    pub fn init0(&mut self, tmp : File){
+        self.fragmentFolder = tmp;
     }
 
     pub fn run(){
@@ -50,8 +55,8 @@ impl FragmentManager(){
         if self.serverIP.len() == 0 {
             return false;
         }
-        if let Ok(connect_socket) = TcpStream::connect((&self.server_ip[..], self.control_port)) {
-            self.to_server = Some(connect_socket);//忽略了setKeepAlieve和setsoTimeout，未找到rust中对应的长连接和超时连接的处理函数
+        if let Ok(connect_socket) = TcpStream::connect((&self.serverIP[..], self.controlPort)) {
+            self.toServer = Some(connect_socket);//忽略了setKeepAlieve和setsoTimeout，未找到rust中对应的长连接和超时连接的处理函数
             println!("Connect to server successfully(data)!");
             if self.Type == 1 {
                 status = self.sendFragment();
@@ -67,7 +72,7 @@ impl FragmentManager(){
         return status;
     }
 
-    pub fn init(&mut self, f : Path, ip : String, port : i32) {
+    pub fn init(&mut self, f : File, ip : String, port : i32) {
         self.fragmentFolder = f;
         self.serverIP = ip;
         self.serverPort = port;
@@ -86,12 +91,11 @@ impl FragmentManager(){
         //let s = self.fragmentFolder + "/" + self.fragmentID;
         //可能会根据运行平台的不同添加/,分为posix和windows
         //一个问题是无法获取文件的绝对路径，没有相应的Path方法
-        let s = self.fragmentID.to_String;
-        let path = Path::new(s);
+        let s = self.fragmentID.to_string();
+        let path = Path::new(&s);
         let mut f = File::create(path);
-        if !f.ok() {
+        if !f.is_ok() {//存疑
             panic!("Error happens on File");
-            return false;
         }
 
         //@SuppressWarnings("deprecation")
@@ -108,7 +112,7 @@ impl FragmentManager(){
                 let mut status = true;
                 //let mut status = FileTransporter.sendFile 需要另一个函数FileTransporter
                 if status {
-                    inFromServer.read_line(&mut sentense).unwrap();
+                    self.inFromServer.read_line(&mut sentense).unwrap();
                     if !sentense.eq(&recv) {
                         return false;
                     }
@@ -120,10 +124,10 @@ impl FragmentManager(){
     }
 
     fn recvFragment(&mut self) -> bool {
-        let s = self.fragmentID.to_String;
-        let path = Path::new(s);
+        let s = self.fragmentID.to_string();
+        let path = Path::new(&s);
         let mut f = File::create(path);
-        if f.ok() {
+        if f.is_ok() {
             remove_file(path);//路径的问题需要调试
         }
         match &mut self.toServer {
@@ -139,10 +143,10 @@ impl FragmentManager(){
     }
 
     fn deleteFragment(&mut self) -> bool {
-        let s = self.fragmentID.to_String;
-        let path = Path::new(s);
+        let s = self.fragmentID.to_string();
+        let path = Path::new(&s);
         let mut f = File::create(path);
-        if f.ok() {
+        if f.is_ok() {
             remove_file(path);
         }
 
--- temp/records/pqz.md
+++ temp/records/pqz.md
@@ -4,4 +4,5 @@
   * @SuppressWarnings("deprecation")
   * 函数后面+throws Exception
 * 关于Rust File和Path中的方法搞得不是很懂，原Java程序中的FragmentFolder和fragmentID分别对应要创建的文件路径和文件名，但是感觉在Rust中应该主要使用Path类，以及文件路径在不同系统上的表示有待进一步研究
+* 对于if语句中的is_ok存疑
 * 其他小的问题在注释中给出了
