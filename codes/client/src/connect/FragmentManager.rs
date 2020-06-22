ca3ce6a1e6edb2af09b54a9def8481f5a19c15f5
pqz <peiqz@mail.ustc.edu.cn>
2020-06-22 18:48:05 +0800

    debug FragmentManger.rs

--- codes/client/src/connect/FragmentManager.rs
+++ codes/client/src/connect/FragmentManager.rs
@@ -1,3 +1,5 @@
+mod FileTransporter;
+
 //use std::net::{TcpStream, Ipv4Addr, Shutdown};
 use std::net::TcpStream;
 use std::string::String;
@@ -8,18 +10,19 @@ use std::io::prelude::*;
 use std::fs::{File, remove_file};
 //use std::ptr::null;
 use std::path::Path;
+use std::path::PathBuf;
 //use std::option::NoneError;
 use std::option::Option;
 
 
+
 struct FragmentManager{
-    fragmentFolder : File,
+    fragmentFolder : String,
     serverIP : String,
     serverPort : i32,
     controlPort : u16,
     toServer : Option<TcpStream>,
     inFromServer : BufReader<TcpStream>,
-    //DateOut/inputStream ??
     requestID : i32,
     fragmentID : i32,
     Type : i32,
@@ -27,9 +30,9 @@ struct FragmentManager{
 
 
 impl FragmentManager {
-    pub fn new(rId : i32, fId : i32, t : i32, f : File, l : TcpStream)->FragmentManager{
+    pub fn new(rId : i32, fId : i32, t : i32, l : TcpStream)->FragmentManager{
         FragmentManager{
-            fragmentFolder : f,
+            fragmentFolder : String :: new(),
             serverIP : String :: new(),
             serverPort : -1,
             controlPort : 0,
@@ -41,9 +44,11 @@ impl FragmentManager {
         }
     }
 
-    pub fn init0(&mut self, tmp : File){
+    /*pub fn init0(&mut self, tmp : String, ip : String, port : i32){
         self.fragmentFolder = tmp;
-    }
+        self.serverIP = ip;
+        self.serverPort = port;
+    }*/
 
     pub fn run(){
         // 暂不进行并发数据操作
@@ -57,6 +62,13 @@ impl FragmentManager {
         }
         if let Ok(connect_socket) = TcpStream::connect((&self.serverIP[..], self.controlPort)) {
             self.toServer = Some(connect_socket);//忽略了setKeepAlieve和setsoTimeout，未找到rust中对应的长连接和超时连接的处理函数
+            match &mut self.toServer {
+                None => println!("Error"),
+                Some(socket) => {
+                    let socket_read = socket.try_clone().expect("clone failed");
+                    let mut inFromServer = BufReader :: new(socket_read);
+                }
+            }
             println!("Connect to server successfully(data)!");
             if self.Type == 1 {
                 status = self.sendFragment();
@@ -72,31 +84,31 @@ impl FragmentManager {
         return status;
     }
 
-    pub fn init(&mut self, f : File, ip : String, port : i32) {
+    pub fn init(&mut self, f : String, ip : String, port : i32) {
         self.fragmentFolder = f;
         self.serverIP = ip;
         self.serverPort = port;
-        match &mut self.toServer {
+        /*match &mut self.toServer {
             None => println!("Error"),
             Some(socket) => {
                 let socket_read = socket.try_clone().expect("clone failed");
                 let mut inFromServer = BufReader :: new(socket_read);
             }
-        }
+        }*/
     }
     //以下函数未实现throw exceptions
     fn sendFragment(&mut self) -> bool {
         let mut status = true;
         let mut sentense = String :: new();
-        //let s = self.fragmentFolder + "/" + self.fragmentID;
+        let mut pathBuf = PathBuf::new();
+        pathBuf.push(&self.fragmentFolder);
+        pathBuf.push("/");
+        pathBuf.push(&self.fragmentID.to_string());
         //可能会根据运行平台的不同添加/,分为posix和windows
-        //一个问题是无法获取文件的绝对路径，没有相应的Path方法
-        let s = self.fragmentID.to_string();
-        let path = Path::new(&s);
-        let mut f = File::create(path);
-        if !f.is_ok() {//存疑
+        let mut f = File::create(pathBuf).unwrap();
+        /*if !f.is_ok() {//如何判断一个文件是否存在？
             panic!("Error happens on File");
-        }
+        }*/
 
         //@SuppressWarnings("deprecation")
         match &mut self.toServer {
@@ -109,12 +121,13 @@ impl FragmentManager {
                 if !sentense.eq(&recv) {
                     return false;
                 }
-                let mut status = true;
+                let socket1 = socket.try_clone().expect("clone failed");//克隆端口
+                let mut status : bool = FileTransporter::send_file(f, socket1);
                 //let mut status = FileTransporter.sendFile 需要另一个函数FileTransporter
                 if status {
                     self.inFromServer.read_line(&mut sentense).unwrap();
                     if !sentense.eq(&recv) {
-                        return false;
+                        status = false;
                     }
                 }
             }
@@ -124,32 +137,40 @@ impl FragmentManager {
     }
 
     fn recvFragment(&mut self) -> bool {
-        let s = self.fragmentID.to_string();
-        let path = Path::new(&s);
-        let mut f = File::create(path);
-        if f.is_ok() {
-            remove_file(path);//路径的问题需要调试
-        }
+        let mut s = String:: new();
+        s.push_str(&self.fragmentFolder);
+        s.push('/');
+        s.push_str(&self.fragmentID.to_string());
+        let mut path = Path::new(&s);
+        //可能会根据运行平台的不同添加/,分为posix和windows
+        let mut f = File::create(path).unwrap();
+        remove_file(path);
         match &mut self.toServer {
             None => println!("Error"),
             Some(socket) => {
-                socket.write_fmt(format_args!("{} {} {}\n", self.Type, self.requestID, self.fragmentID));
-                socket.flush();
-                //if(FIleTransporter.recvFIle) 调用另一个函数
+                let socket1 = socket.try_clone().expect("clone failed");//克隆端口
+                if (FileTransporter::recv_file(f, socket1)){
+                    socket.write_fmt(format_args!("{} {} {}\n", self.Type, self.requestID, self.fragmentID));
+                    socket.flush();
+                    return true;
+                }else {
+                    return false;
+                }
+
             }
         }
-        return true;//为了通过编译加的一句，后续删除
-
+        return true;//不知道为什么最后不加返回值就报错
     }
 
     fn deleteFragment(&mut self) -> bool {
-        let s = self.fragmentID.to_string();
-        let path = Path::new(&s);
-        let mut f = File::create(path);
-        if f.is_ok() {
-            remove_file(path);
-        }
-
+        let mut s = String:: new();
+        s.push_str(&self.fragmentFolder);
+        s.push('/');
+        s.push_str(&self.fragmentID.to_string());
+        let mut path = Path::new(&s);
+        //可能会根据运行平台的不同添加/,分为posix和windows
+        let mut f = File::create(path).unwrap();
+        remove_file(path);
         match &mut self.toServer {
             None => println!("Error"),
             Some(socket) => {
@@ -167,18 +188,13 @@ impl FragmentManager {
             }
 
         }
-        return true;//不知道为什么最后不加返回值就报错，后续需要处理
-
-
+        return true;//不知道为什么最后不加返回值就报错
     }
 
 
     fn errorHandler(Type : i32){
         return;
     }
-
-
-
+    
 }
-
 
